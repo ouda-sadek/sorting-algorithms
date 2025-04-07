23302350bf6cae65cbe95d32f501b9df98ea6c84
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-04-07 15:35:11 +0200

    in progress

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sorting import SortNumbers
+from perf_measures import Performance
 
 def main():
     algo = SortNumbers()  
+    board = Performance()
 
-    algo.generate_random_list(taille=600, min_val=0.0, max_val=500000.0)
+    data= algo.generate_random_list(taille=2000, min_val=0.0, max_val=500000.0)
 
     print("Before sort: ")
     algo.display()  
@@ -20,15 +22,12 @@ def main():
 
     print("\nMeasurement of execution times for each algorithm:")
     for name, algorithm in algorithms:
-        # Créer une nouvelle copie de la liste avant chaque appel d'algorithme
-        lst_copy = algo.arr.copy()  
-
-        # Passer la copie à measure_time
-        lst_sorted, time_taken = algo.measure_time(algorithm, lst_copy)  
-        print(f"{name}: {time_taken:.6f} seconds")  
-
-# print(f"Sorted list: {lst_sorted}")
+       
+        sorted_data = board.measure_performance(algorithm, data, name)
+       
     
+    board.display_tests()
+# print(f"Sorted list: {lst_sorted}"    
         
 
 if __name__ == "__main__":
